ca8a824ca989d640cd8c84f88bac2f8efd2f7580
kiss9815 <kiss9815@naver.com>
2019-01-02 00:32:05 +0900

    jsp 와 thymeleaf 동시적용

--- src/main/java/com/boilerPlate/app/Controller/MainController.java
+++ src/main/java/com/boilerPlate/app/Controller/MainController.java
@@ -1,4 +1,4 @@
-package com.boilerPlate.app;
+package com.boilerPlate.app.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +12,12 @@ public class MainController {
 	@RequestMapping(value="/main", method=RequestMethod.GET) 
 	public String main() {
 		
-		return "main";
+		return "thymeleaf/main";
+	}
+	
+	@RequestMapping(value="/jsp", method=RequestMethod.GET) 
+	public String jsp() {
+		
+		return "jsp/sample";
 	}
 }
--- src/main/java/com/boilerPlate/app/SpringBootBoilerPlateApplication.java
+++ src/main/java/com/boilerPlate/app/SpringBootBoilerPlateApplication.java
@@ -1,15 +1,20 @@
 package com.boilerPlate.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+	
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})  // Datasource설정은 자동설정에서 제외 
+@ComponentScan(basePackages = "com.boilerPlate.app")
 public class SpringBootBoilerPlateApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {			
 		SpringApplication.run(SpringBootBoilerPlateApplication.class, args);
 		System.out.println("main start");
-	}
+	}	
 
 }
 

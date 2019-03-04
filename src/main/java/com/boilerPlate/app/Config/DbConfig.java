651ed46b544fabfd5a335f0d87c06e78c1195e52
juntae <juntae@juntae-asus>
2019-03-04 22:36:34 +0900

    계정정보 삭제

--- src/main/java/com/boilerPlate/app/Config/DbConfig.java
+++ src/main/java/com/boilerPlate/app/Config/DbConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import Util.DbInfo;
+
 @Configuration
 public class DbConfig {
 
@@ -15,9 +17,10 @@ public class DbConfig {
      public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/boilerPlate?useSSL=false&characterEncoding=UTF-8&serverTimezone=UTC");
-        dataSource.setUsername( "root" );
-        dataSource.setPassword( "11" );
+        //dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/boilerPlate?useSSL=false&characterEncoding=UTF-8&serverTimezone=UTC");
+        dataSource.setUrl(DbInfo.DbServerUrl);
+        dataSource.setUsername(  DbInfo.DbUser);
+        dataSource.setPassword( DbInfo.DbPassword );
         return dataSource;
      }
 }
